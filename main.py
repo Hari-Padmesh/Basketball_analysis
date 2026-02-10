1a29c94f5e96b49d56ec245bda099a09bbc82771
Hari Padmesh <kanagarajharipadmesh@gmail.com>
2026-02-10 16:11:25 +0530

    Added Checkpoint logic using stubs

--- main.py
+++ main.py
@@ -1,34 +1,20 @@
-from ultralytics import YOLO
-import os
+from utils import read_video, save_video    
+from trackers import playerTracker
+def main():
+    video_frame=read_video("input_videos/video_1.mp4")
+    #initialise Tracker
+    player_tracker= playerTracker("models/player_detector.pt")
 
+    #Run Trackers
+    player_tracks= player_tracker.get_object_tracks(video_frame,
+                                                    read_from_stub=True,
+                                                stub_path="stubs/player_track_stubs.pkl")
 
+    print(player_tracks) 
 
-model = YOLO("models/ball_detector_model.pt")
-source_path = "input_videos/video_1.mp4"
-output_root = r"D:\Basketball_analysis\runs"
-result = model.predict(
-    source=source_path,
-    save=True,
-    project=output_root,
-    name="basketball_output",
-    exist_ok=True,
-)
+    save_video(video_frame,"output_videos/output_video.avi")   
 
-print(result)
-print("============")
-# for box in result[0].boxes:
-#     print(box)
-save_dir = result[0].save_dir
-print(save_dir)
+    
 
-# Rename the output to a fixed filename inside the runs folder.
-original_name = os.path.basename(source_path)
-src_video = os.path.join(save_dir, original_name)
-target_video = os.path.join(save_dir, "annotated_video.mp4")
-if os.path.exists(src_video):
-    if os.path.exists(target_video):
-        os.remove(target_video)
-    os.replace(src_video, target_video)
-    print(f"Annotated video saved to: {target_video}")
-else:
-    print(f"Could not find output video at: {src_video}")
+if __name__ == "__main__":
+    main()
